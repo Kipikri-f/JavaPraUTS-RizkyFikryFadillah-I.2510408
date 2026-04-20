53503698dcf0bb5b9c4e199dcdda0a6d8f80f594
FikryRizky <you@example.com>
2026-04-20 20:43:31 +0700

    Done✅

--- Pra2/I2TugasAbstraksi2/Kendaraan.java
+++
@@ -1,5 +0,0 @@
-package Pra2.I2TugasAbstraksi2;
-
-abstract class Kendaraan {
-    abstract void bergerak();
-}
--- Pra2/I2TugasAbstraksi2/Mobil.java
+++
@@ -1,7 +0,0 @@
-package Pra2.I2TugasAbstraksi2;
-
-class Mobil extends Kendaraan {
-    void bergerak() {
-        System.out.println("Mobil bergerak dengan roda");
-    }
-}
